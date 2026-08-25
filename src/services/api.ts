b2b48228fe0b4d3af8ebd30076c3d97f1c6d7f35
Omphile Lucas <lucasomphile23@gmail.com>
2026-08-25 22:44:10 +0200

    Set up axios for requests to json database

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// This points to our JSON Server
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    headers
+        : {
+        "Content-Type": "application/json",
+    },
+});
