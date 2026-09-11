535472c90a02c573cdc9cdf898c2b78eb2876889
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-11 09:35:36 +0200

    updated api to prevent hanging requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// This points to our JSON Server
+// This points to Render deployment
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers
-        : {
+    baseURL: import.meta.env.VITE_API_URL, 
+    headers: {
         "Content-Type": "application/json",
     },
+    timeout: 10000, 
 });
 
 export default api;
