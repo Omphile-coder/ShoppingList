8704778cd2a25b0f87eb4f2fab415a80d3ad6954
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-17 12:03:59 +0200

    Added the image Redux state/API integration to existing Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
 import authReducer from "../features/auth/authSlice";
 import shoppingListReducer from "../features/shoppingLists/shoppingListsSlice";
-import shoppingItemReducer  from "../features/shoppingLists/shoppingItemSlice";
+import shoppingItemReducer from "../features/shoppingLists/shoppingItemSlice";
+import { imageApi } from "../api/imageApi";
 
 export const store = configureStore({
- 
-    reducer: {
-        auth: authReducer, 
-        shoppingLists: shoppingListReducer,
-        shoppingItems: shoppingItemReducer,
-    },
-  
+  reducer: {
+    auth: authReducer,
+    shoppingLists: shoppingListReducer,
+    shoppingItems: shoppingItemReducer,
+    [imageApi.reducerPath]: imageApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(imageApi.middleware),
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
